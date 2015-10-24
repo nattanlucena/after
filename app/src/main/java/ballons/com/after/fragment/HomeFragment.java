cb7a9547ef999ea3b27df9a55b24e5e65b757ea4
Nattan Lucena <nattanlucena@gmail.com>
2015-10-24 00:34:04 -0300

    feed implementation

--- app/src/main/java/ballons/com/after/fragment/HomeFragment.java
+++ app/src/main/java/ballons/com/after/fragment/HomeFragment.java
@@ -8,14 +8,39 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import ballons.com.after.R;
+import ballons.com.after.adapter.FeedListAdapter;
+import ballons.com.after.app.AppController;
+import ballons.com.after.model.FeedItem;
 
 
 public class HomeFragment extends Fragment {
 
-    private Context mContex;
+    private static String TAG = HomeFragment.class.getSimpleName();
+    private ListView mListView;
+    private FeedListAdapter mFeedListAdapter;
+    private List<FeedItem> mFeedItems;
+    private String URL_FEED = "http://api.androidhive.info/feed/feed.json";
+
 
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
@@ -33,14 +58,83 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        mListView = (ListView) rootView.findViewById(R.id.feed_list);
+
+        mFeedItems = new ArrayList<FeedItem>();
+
+        mFeedListAdapter = new FeedListAdapter(getActivity(), mFeedItems);
+        mListView.setAdapter(mFeedListAdapter);
+
+        Cache mCache = AppController.getInstance().getRequestQueue().getCache();
+        Cache.Entry entry = mCache.get(URL_FEED);
+
+        if( entry != null){
+            try{
+                String data = new String(entry.data, "UTF-8");
+                try{
+                    parseJsonFeed(new JSONObject(data));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else{
+
+            JsonObjectRequest jsoReq = new JsonObjectRequest(Request.Method.GET, URL_FEED, null, new Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    VolleyLog.d(TAG, "Response: " + response.toString());
+                    if (response != null) {
+                        parseJsonFeed(response);
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error: " + error.getMessage());
+                }
+            });
+
+            AppController.getInstance().addToRequestQueue(jsoReq);
+        }
+
         return rootView;
     }
 
-    public Context getContext(){
-        return mContex;
+
+    private void parseJsonFeed(JSONObject response) {
+        try {
+            JSONArray feedArray = response.getJSONArray("feed");
+
+            for (int i = 0; i < feedArray.length(); i++) {
+                JSONObject feedObj = (JSONObject) feedArray.get(i);
+
+                FeedItem item = new FeedItem();
+                item.setId(feedObj.getInt("id"));
+                item.setName(feedObj.getString("name"));
+
+                // Image might be null sometimes
+                String image = feedObj.isNull("image") ? null : feedObj
+                        .getString("image");
+                item.setImage(image);
+                item.setStatus(feedObj.getString("status"));
+                item.setProfilePic(feedObj.getString("profilePic"));
+                item.setTimeStamp(feedObj.getString("timeStamp"));
+
+                // url might be null sometimes
+                String feedUrl = feedObj.isNull("url") ? null : feedObj
+                        .getString("url");
+                item.setUrl(feedUrl);
+
+                mFeedItems.add(item);
+            }
+
+            // notify data changes to list adapater
+            mFeedListAdapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setContext(Context context){
-        this.mContex = context;
-    }
 }
